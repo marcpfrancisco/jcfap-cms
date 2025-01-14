0f6dc7f02ff0d22694316b1a4388b808ec9c8fa5
Marc Francisco <marcpfrancisco@gmail.com>
2025-01-14 16:07:45 +0800

    APP:
    - Added nextjs google third parties packages
    - Added google tag manager in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 import LayoutWrapper from "@/components/Layout";
 import "./globals.css";
@@ -23,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <GoogleTagManager
+        gtmId="G-4XDYR6WFE3"
+        gtmScriptUrl="https://www.googletagmanager.com/gtag/js?id=G-4XDYR6WFE3"
+      />
       <body className={`${inter.variable} antialiased`}>
         <Toaster />
         <LayoutWrapper>{children}</LayoutWrapper>
